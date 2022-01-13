20a6387cd7ce8ff9fbc5e729c85a2b48b8e1e021
malgo-slota <malgo.slota@gmail.com>
2022-01-13 21:51:23 +0100

    styling and functionality

---
+++ app.js
@@ -0,0 +1,12 @@
+const accordion = document.getElementsByClassName('accordion-item');
+
+for (i=0; i<accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')        
+  })
+}
+
+ 
+
+       
+    
